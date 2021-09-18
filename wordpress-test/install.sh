6303fe6d71818e53aa62167c63071a59dd8e64b6
HieuNT <hieunt9@gmail.com>
2021-09-18 18:31:57 +0700

    update 18Sep2021

--- wordpress-test/install.sh
+++ wordpress-test/install.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# add repo
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
+
+# install
 kubectl create ns wordpress-test
 helm install wp-test bitnami/wordpress -n wordpress-test -f values.yaml
