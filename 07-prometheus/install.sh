22ba9ff6d26928e88b4df1aab052f3e251a72c0c
HieuNT <hieunt9@gmail.com>
2021-09-20 16:53:30 +0700

    update 20Sep2021

--- 07-prometheus/install.sh
+++ 07-prometheus/install.sh
@@ -6,14 +6,14 @@ helm repo update
 
 # install chart
 kubectl create ns prometheus
-kubectl config set-context --current prometheus
+kubectl config set-context --current --namespace prometheus
 helm install prometheus prometheus-community/kube-prometheus-stack \
 -n prometheus \
 -f ./values.yaml \
 --version "18.0.10"
 
 # install ingress for grafana service
-kubectl config set-context --current prometheus
+kubectl config set-context --current --namespace prometheus
 kubectl apply -f ./ingress-grafana.yaml
 
 
